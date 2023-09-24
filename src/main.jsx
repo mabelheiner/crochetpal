92a74c4e4b01f3165f8038e439cc3466a164fd5e
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2023-09-24 00:23:42 -0600

    changed the structure of the router

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Project from './Project.jsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter} from 'react-router-dom' 
+import React from 'react';
+import { createRoot } from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import the correct components
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />
-  },
-  {
-    path: '/project',
-    element: <Project />
-  }
-])
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from './App.jsx';
+import Project from './Project.jsx';
+import './index.css';
+
+const root = document.getElementById('root');
+const app = (
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes> {/* Use Routes component to define your routes */}
+        <Route path="/" element={<App />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+createRoot(root).render(app); // Use createRoot to render your app

0810bd6f02e944777edd520644a0bd0b6bf56432
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2023-09-23 18:09:59 -0600

    fixed logo link on the front page

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <header>
-        <img class='logo' src="./public/images/CrochetPal.png" alt="Crochet Pal Logo" />
+        <img class='logo' src="./images/CrochetPal.png" alt="Crochet Pal Logo" />
         <h1>Welcome to CrochetPal</h1>
       </header>
     </>

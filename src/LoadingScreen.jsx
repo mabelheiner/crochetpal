3d9b55185c5eef880e8d6f8acb092ee3e8e1cca2
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2023-11-17 16:58:05 -0700

    I created a gif that could work, just comment the code with the gif if you want to switch and uncomment the image code

--- src/LoadingScreen.jsx
+++ src/LoadingScreen.jsx
@@ -3,7 +3,8 @@ import './LoadingScreen.css';
 const LoadingScreen = () => {
     return (
         <>
-        <img className='loaderimage' src='/images/CrochetPal-Logo.png' alt='CrochetPal'></img>
+        {/* <img className='loaderimage' src='/images/CrochetPal-Logo.png' alt='CrochetPal'></img> */}
+        <img  className='loaderimage' src='/public/images/Yarn-Loader.gif' alt='Yarn Turning'></img>
         <h1>Please Wait</h1>
         </>
     )}

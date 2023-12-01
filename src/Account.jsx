47045008e512a5588602b113e373b5a25a8f53ef
Darcy Merilan <91292053+Darcymer@users.noreply.github.com>
2023-11-30 20:32:28 -0700

    fotter changes and pics uploaded

--- src/Account.jsx
+++ src/Account.jsx
@@ -67,6 +67,7 @@ const Account = () => {
         ): (
             <Login />
         )}
+        
         </>
     )
 }
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,6 +3,8 @@ import './Footer.css';
 const Footer = () => {
     return (
         <>
+        <br />
+        <div className='back'>
         <div className='footer'>
             <p>&copy;CrochetPal</p>
             < a href='https://www.instagram.com' target='_blank' rel='noopener noreferrer'>
@@ -16,7 +18,7 @@ const Footer = () => {
             < a href='https://www.twitter.com' target='_blank' rel='noopener noreferrer'>
                 <img src='\images\twitter.png' alt='Twitter'></img>
             </a>
-            
+          </div>  
         </div>
       </>
     )
